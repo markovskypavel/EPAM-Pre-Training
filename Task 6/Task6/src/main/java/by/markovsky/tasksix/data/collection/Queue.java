382bf390e064dc4f2942dd7c453705a9832cfe20
Pavel Markovsky <markovskypavel@gmail.com>
2018-02-23 20:51:34 +0300

    Deleted project

--- Task 6/Task6/src/main/java/by/markovsky/tasksix/data/collection/Queue.java
+++
@@ -1,6 +0,0 @@
-package by.markovsky.tasksix.data.collection;
-
-public interface Queue<E> extends CustomCollection<E> {
-    boolean enqueue(E element);
-    E dequeue();
-}
--- Task 6/Task6/src/main/java/by/markovsky/tasksix/data/collection/Stack.java
+++
@@ -1,6 +0,0 @@
-package by.markovsky.tasksix.data.collection;
-
-public interface Stack<E> extends CustomCollection<E> {
-    boolean push(E element);
-    E pop();
-}
